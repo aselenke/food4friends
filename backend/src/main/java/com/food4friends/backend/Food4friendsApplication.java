2439d490ba36e9717035528bbbc4bafe2b451c2e
Abraham Selenke <aselenke96@gmail.com>
2025-04-05 15:26:00 -0600

    Added spring boot init setup

---
+++ backend/src/main/java/com/food4friends/backend/Food4friendsApplication.java
@@ -0,0 +1,13 @@
+package com.food4friends.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Food4friendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Food4friendsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/food4friends/backend/Food4friendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food4friends.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Food4friendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
